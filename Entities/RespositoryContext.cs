c38fd5cc522151c18d35eeef435a55ae24c3a75c
afolesmith <afolesmith@gmail.com>
2021-08-17 08:47:58 +0100

    dbcontext

---
+++ Entities/RespositoryContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class RespositoryContext : DbContext
+    {
+        public RespositoryContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
